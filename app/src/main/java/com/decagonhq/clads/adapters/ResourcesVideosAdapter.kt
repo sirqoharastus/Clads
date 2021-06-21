4645b1fba56f557daa12f8bc019945d0b9c9dedd
Jesse <jesseonoyeyan@yahoo.com>
2021-06-21 19:12:13 +0100

    Created resources fragment that contains a list of videos and articles

---
+++ app/src/main/java/com/decagonhq/clads/adapters/ResourcesVideosAdapter.kt
@@ -0,0 +1,41 @@
+package com.decagonhq.clads.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.decagonhq.clads.R
+import com.decagonhq.clads.databinding.FragmentResourcesVideoRecyclerviewItemBinding
+import com.decagonhq.clads.models.VideoView
+
+class ResourcesVideosAdapter(private var videoList: ArrayList<VideoView>) : RecyclerView.Adapter<ResourcesVideosAdapter.VideoViewHolder>() {
+
+    inner class VideoViewHolder(var binding: FragmentResourcesVideoRecyclerviewItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        var videoLink = binding.resourcesVideoRecyclerviewExoplayerPlayerview
+        var videoTag = binding.resourceRecyclerviewSewingTextview
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoViewHolder {
+        val binding = FragmentResourcesVideoRecyclerviewItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return VideoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
+        holder.itemView.apply {
+            with(holder) {
+                with(videoList[position]) {
+                    videoLink.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.home))
+                    videoTag.text = videoTitle
+                }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = videoList.size
+}
